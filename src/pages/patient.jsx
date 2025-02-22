526e95976d9098c4576f7c4f396f911a2d511f55
basit <basatali326@gmail.com>
2025-02-22 11:46:25 +0400

    solve

--- src/pages/patient.jsx
+++ src/pages/patient.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { InlineWidget } from "react-calendly";
-import Layout from "../components/Layout/Layout";
+// import Layout from "../components/Layout/Layout";
 
 const Patient = () => {
   const { data: session, status } = useSession();
@@ -16,12 +16,10 @@ const Patient = () => {
   }, [status, session, router]);
 
   return (
-    <Layout>
-      <div className="container">
-        <h1>Book Slot To Meet Doctor!</h1>
-        <InlineWidget url="https://calendly.com/geeklies-agency/test?preview_source=et_card&month=2025-02" />
-      </div>
-    </Layout>
+    <div className="container">
+      <h1>Book Slot To Meet Doctor!</h1>
+      <InlineWidget url="https://calendly.com/geeklies-agency/test?preview_source=et_card&month=2025-02" />
+    </div>
   );
 };
 
